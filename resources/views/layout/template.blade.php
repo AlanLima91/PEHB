ccbad4da33c259b234eba20c6cf74dd323c05755
AlexandreLac <alexandrelac248@gmail.com>
2016-11-14 22:12:02 +0100

    Modification de navbar

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -66,33 +66,25 @@
         </style>
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-		  <div class="container">
-		    <div class="navbar-header">
+		<nav class="navbar navbar-inverse">
+          <div class="container">
+            <div class="navbar-header">
 
-		    	<!--Met en place un bouton en haut à droite-->
-		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/')}}">PEHB</a>
-		    </div>
-		    <div class="collapse navbar-collapse" id="app-navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('actu') }}">Actualités</a></li>
-				</ul>
-	            <ul class="nav navbar-nav">
-	            	<li><a href="{{ url('equipe') }}">équipes</a></li>
-	            </ul>
-	            <ul class="nav navbar-nav">
-	            	<li><a href="{{ url('calendrier') }}">calendrier</a></li>
-	            </ul>
-	            <ul class="nav navbar-nav">
-	            	<li><a href="{{ url('resultat') }}">Résultats</a></li>
-	            </ul>
+                <!--Met en place un bouton en haut à droite-->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url('/')}}">PEHB</a>
+            </div>
+            <div class="collapse navbar-collapse" id="nav">
                 <ul class="nav navbar-nav">
+                    <li><a href="{{ url('actu') }}">Actualités</a></li>
+                    <li><a href="{{ url('equipe') }}">équipes</a></li>
+                    <li><a href="{{ url('calendrier') }}">calendrier</a></li>
+                    <li><a href="{{ url('resultat') }}">Résultats</a></li>
                     <li><a href="{{ url('information') }}">Informations</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
@@ -122,10 +114,15 @@
                         </li>
                     @endif
                 </ul>
-        	</div>
-		  </div>
-		</nav>
-		<script type="text/javascript" src="https://cdn.datatables.net:r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
-		@yield("content")
-	</body>
+            </div>
+          </div>
+        </nav>
+        @yield("content")
+    </body>
+	<script type="text/javascript" src="https://cdn.datatables.net:r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
