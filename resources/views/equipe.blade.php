d8106d266ddf2909b2b2cad52f95fe79577ebe79
AlanLima91 <m.limaalan@hotmail.fr>
2016-12-12 18:06:12 +0100

    Modification visuelle

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -4,7 +4,7 @@
 <a href="{{ url('equipe/Ajoutcategorie') }}" class="btn btn-primary btn-lg active pull-right" style="margin-bottom: 15px;margin-right: 15px;" role="button">Crée une catégorie</a>
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-md-offset-2">
+        <div class="col-md-12 col-md-offset-0" style="text-align:center">
             <div class="panel panel-default">
                 <div class="panel-heading"><h3>Les catégories</h3></div>
                 	<div class="panel-body">
@@ -16,7 +16,7 @@
                                     <div class="panel-body">
                                         <a href="{{ url('equipe/Ajoutequipe') }}" class="btn btn-primary btn-lg active pull-right" style="margin-bottom: 15px;margin-right: 10px;" role="button">Crée une équipe</a>
                                         @foreach($Equipe as $key)
-                                            <div class="col-md-10 col-md-offset-1">
+                                            <div class="col-md-10 col-md-offset-0">
                                                 <div class="panel panel-default">
                                                     <div class="panel-heading"><h5>{{$key->name}}</h3>
                                                     </div>
